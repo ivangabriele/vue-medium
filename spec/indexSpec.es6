5bbd3b814da0fa945c9859f81fc210ce58d17146
Ivan Gabriele <ivan.gabriele@gmail.com>
2017-03-10 09:21:54 +0100

    test: remove Jasmine specs

--- spec/indexSpec.es6
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import VueMedium from '../src'
-
-describe('VueMedium', () => {
-  it('has a created hook', () => {
-    expect(typeof VueMedium.install).toBe('function')
-  })
-})
--- spec/indexSpec.js
+++
@@ -1,8 +0,0 @@
-const Vue = require('vue')
-const VueMedium = require('../dist/index.umd')
-
-describe('VueMedium', () => {
-  it('has a created hook', () => {
-    expect(typeof VueMedium.install).toBe('function')
-  })
-})
